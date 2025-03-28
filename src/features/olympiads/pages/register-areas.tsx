cd3a6bc3c5744bfcab21f459d5695bb19fdbf1a2
Bruno Vega <brunovega123407@gmail.com>
2025-03-28 12:56:54 -0400

    style: add footer design

--- src/features/olympiads/pages/register-areas.tsx
+++ src/features/olympiads/pages/register-areas.tsx
@@ -1,11 +1,20 @@
+import FooterDesign from '../../../components/footer-design';
 import FormAreas from '../components/form-areas';
 
 export const RegisterAreas = () => {
   return (
-    <div className=" min-h-screen flex flex-col bg-white">
+    <div className="min-h-screen flex flex-col bg-white">
       <main className="flex-grow">
-        <FormAreas />
+        <div className="flex flex-col">
+          <div className="z-10">
+            <FormAreas />
+          </div>
+        </div>
       </main>
+      {/* El footer se mantiene al final con flexbox */}
+      <div className="z-0">
+        <FooterDesign />
+      </div>
     </div>
   );
 };
