ca7002dbeb924a97fd8f0e852aa2d8da32a719b1
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2025-03-29 15:21:22 -0400

    refactor: clean up App component and remove unused state 🧹✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react';
 import { RegisterAreas } from './features/olympiads/pages/register-areas';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
         {/*<Register/>*/}
-        <RegisterAreas/>
+        <RegisterAreas />
       </div>
     </>
   );
