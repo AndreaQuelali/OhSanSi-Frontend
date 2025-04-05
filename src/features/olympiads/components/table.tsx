e9031a9f0be3aaefcbb63953008098923f4a6c00
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-04-05 07:04:48 -0400

    feat: add pagination to Table

--- src/features/olympiads/components/table.tsx
+++ src/features/olympiads/components/table.tsx
@@ -1,41 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DataTable, { TableColumn } from 'react-data-table-component';
+import { Button } from '@/components';
 
-interface Table {
+interface TableRow {
   id: number;
   area: string;
   level: string;
   grade: string;
 }
 
-type TableRow = {
-  id: number;
-  area: string;
-  level: string;
-  grade: string;
-};
-
 type TableProps = {
   data: TableRow[];
 };
 
 export const Table: React.FC<TableProps> = ({ data }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const paginatedData = data.slice(startIndex, startIndex + itemsPerPage);
+
   const columns: TableColumn<TableRow>[] = [
     {
       name: 'Área',
-      selector: (row: TableRow) => row.area,
+      selector: (row) => row.area,
       sortable: true,
       cell: (row) => <span className="text-onBack body-lg">{row.area}</span>,
     },
     {
       name: 'Nivel/Categoría',
-      selector: (row: TableRow) => row.level,
+      selector: (row) => row.level,
       sortable: true,
       cell: (row) => <span className="text-onBack body-lg">{row.level}</span>,
     },
     {
       name: 'Grados',
-      selector: (row: TableRow) => row.grade,
+      selector: (row) => row.grade,
       sortable: true,
       cell: (row) => <span className="text-onBack body-lg">{row.grade}</span>,
     },
@@ -53,30 +53,57 @@ export const Table: React.FC<TableProps> = ({ data }) => {
   };
 
   return (
-    <div className="w-full flex items-center justify-center my-6">
-      <div className="w-full">
-        <div className="flex justify-between">
-          <h2 className="text-primary subtitle-md">
-            Niveles/Categorías registradas en área
-          </h2>
-        </div>
-        <div className="max-h-[100px] overflow-y-auto">
-          <DataTable
-            title=""
-            columns={columns}
-            data={data}
-            pagination
-            responsive
-            highlightOnHover
-            customStyles={customStyles}
-            noDataComponent={
-              <span className="text-neutral2 body-lg">
-                No hay niveles o categorías registradas
-              </span>
-            }
-          />
-        </div>
-      </div>
+    <div className="w-full flex flex-col items-center justify-center my-6">
+      {data.length === 0 ? (
+        <span className="text-neutral2 body-lg">
+          No hay niveles o categorías registradas en áreas
+        </span>
+      ) : (
+        <>
+          <div className="w-full">
+            <DataTable
+              title=""
+              columns={columns}
+              data={paginatedData}
+              responsive
+              highlightOnHover
+              customStyles={customStyles}
+              noHeader
+            />
+          </div>
+
+          {totalPages > 1 && (
+            <div className="flex justify-center mt-10 flex-wrap gap-2">
+              <Button
+                label="Anterior"
+                onClick={() => setCurrentPage((prev) => prev - 1)}
+                disabled={currentPage === 1}
+                variantColor={
+                  currentPage === 1 ? 'variantDesactivate' : 'variant2'
+                }
+              />
+
+              {[...Array(totalPages)].map((_, i) => (
+                <Button
+                  key={i + 1}
+                  label={`${i + 1}`}
+                  onClick={() => setCurrentPage(i + 1)}
+                  variantColor={currentPage === i + 1 ? 'variant1' : 'variant2'}
+                />
+              ))}
+
+              <Button
+                label="Siguiente"
+                onClick={() => setCurrentPage((prev) => prev + 1)}
+                disabled={currentPage === totalPages}
+                variantColor={
+                  currentPage === totalPages ? 'variantDesactivate' : 'variant2'
+                }
+              />
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 };
