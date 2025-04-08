7c909e6c387e98f2853274b5e429855c6f296543
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2025-04-08 19:29:38 -0400

    feat: add dropdown menu component and update routes for participant registration 🎉📋

---
+++ src/components/ui/dropdown-menu.tsx
@@ -0,0 +1,54 @@
+import { Link, useLocation } from 'react-router';
+import { useState, useRef } from 'react';
+import IconDown from '../icons/icon-down';
+
+type DropdownMenuProps = {
+  label: string;
+  options: { label: string; path: string }[];
+};
+
+export default function DropdownMenu({ label, options }: DropdownMenuProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+  const menuRef = useRef<HTMLLIElement | null>(null);
+
+  return (
+    <li ref={menuRef} className="relative" onClick={() => setIsOpen(!isOpen)}>
+      <span
+        className={`subtitle-sm p-1 cursor-pointer ${
+          options.some((option) => location.pathname.startsWith(option.path))
+            ? 'text-red-500 border-b-[1px] border-b-red-500'
+            : 'text-primary hover:text-secondary'
+        }`}
+      >
+        {label}
+        <IconDown
+          className={`w-4 h-4 inline-block ml-1 transition-transform duration-200 ${
+            isOpen ? 'rotate-180' : 'rotate-0'
+          }`}
+        />
+      </span>
+      {isOpen && (
+        <ul className="absolute top-full left-0 bg-white shadow-lg rounded-md mt-2 w-48">
+          {options.map((option, index) => (
+            <li
+              key={index}
+              className={`hover:bg-gray-100 ${
+                location.pathname === option.path
+                  ? 'text-red-500'
+                  : 'text-primary'
+              }`}
+            >
+              <Link
+                to={option.path}
+                className="block px-4 py-2 text-sm hover:text-secondary"
+              >
+                {option.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </li>
+  );
+}
