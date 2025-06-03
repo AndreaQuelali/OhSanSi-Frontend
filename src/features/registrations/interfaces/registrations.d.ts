9050e6d48758e11b8318c9eba23d71ab9da0f420
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2025-06-02 20:44:17 -0400

    feat: refactor registration and list types for improved clarity and structure ✨📋

--- src/features/registrations/interfaces/registrations.d.ts
+++ src/features/registrations/interfaces/registrations.d.ts
@@ -25,7 +25,46 @@ export type FormData = {
 };
 
 export type RegistrationsListProps = {
-  showGenerateButton?: boolean; // opcional, por defecto false
+  showGenerateButton?: boolean;
   showUploadButton?: boolean;
   title?: string;
+};
+
+export type Registration = {
+  nombre: string;
+  ci: string;
+  area: string;
+  categoria: string;
+};
+
+export type List = {
+  cantidad: number;
+  cantidadOlimpistas: number;
+  responsable: string;
+  ci: string;
+  estado: string;
+  id_lista?: number;
+  tipo: 'individual' | 'grupal';
+};
+
+export type PaymentData = {
+  ci: string;
+  nombres: string;
+  apellidos: string;
+  cantidadOlimpistas: number;
+  total: number;
+  unitario: number;
+  niveles: { nivel_id: number; nombre_nivel: string; area: string }[];
+  totalLiteral: string;
+  fecha: string;
+  hora: string;
+  nroOrden: string;
+};
+
+export type Props = {
+  list: List;
+  registrations: Registration[];
+  isAlternate?: boolean;
+  showGenerateButton?: boolean;
+  showUploadButton?: boolean;
 };
