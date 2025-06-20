44dc72f7d7207e654002f4238b33ee7e5d5dd552
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-06-20 01:17:53 -0400

    style: add icons for step of registrations

---
+++ src/components/icons/icon-arrow.tsx
@@ -0,0 +1,19 @@
+export default function IconArrow() {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 24H38M38 24L24 10M38 24L24 38"
+        stroke="#E51B27"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
