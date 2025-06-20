d6ad4e5b740f0418cc95544f802c5b048344b020
Bruno Vega <brunovega123407@gmail.com>
2025-06-20 01:13:08 -0400

    feat: add new animated text component and card area icons; update header and hero sections

--- src/features/home/pages/hero.tsx
+++ src/features/home/pages/hero.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components';
-import { useNavigate } from 'react-router';
 import { OlympiadInfo } from '@/interfaces/olympiad';
 import {
   formatDate,
@@ -8,147 +7,151 @@ import {
 } from '@/utils/olympiad';
 import { useFetchData } from '@/hooks/use-fetch-data';
 import { PageLoader } from '@/components/ui/loadings';
+import { Header } from './header';
+import { CardArea } from '../components/card-area';
+import { useEffect, useState } from 'react';
+import {
+  AlarmClockCheck,
+  AlarmClockOff,
+  CalendarX,
+} from 'lucide-react';
 
 export const Presentation = () => {
-  const navigate = useNavigate();
   const { data, loading } = useFetchData<OlympiadInfo[]>(
-    `/olympiads/${getCurrentYear()}`,
+    `/olympiads/${getCurrentYear()}/management`
   );
 
   localStorage.setItem('userRole', 'user');
+  const currentOlympiad = data?.[0] || null;
+  const [areasUrl, setAreasUrl] = useState<string | null>(null);
 
-  const currentOlympiad = data && data.length > 0 ? data[0] : null;
+  useEffect(() => {
+    if (currentOlympiad) {
+      setAreasUrl(`/olympiads/${currentOlympiad.olympiad_id}/levels-areas`);
+    }
+  }, [currentOlympiad]);
+
+  const {
+    data: areasData,
+    loading: areasLoading,
+  } = useFetchData<{
+    success: boolean;
+    data: { year: number; areas: { area_id: number; area_name: string }[] };
+  }>(areasUrl || '');
+
+  const statusColors = {
+    'not-started': 'text-yellow-600 bg-yellow-50 border-yellow-200',
+    active: 'text-green-600 bg-green-50 border-green-200',
+    ended: 'text-red-600 bg-red-50 border-red-200',
+  } as const;
+
+  const icons = {
+    'not-started': <AlarmClockOff className="w-5 h-5 animate-pulse" />,
+    active: <AlarmClockCheck className="w-5 h-5 animate-bounce" />,
+    ended: <CalendarX className="w-5 h-5" />,
+  };
+
+  const availableAreas = areasData?.data.areas || [];
+
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "/docs/convocatoria_OhSansi.pdf";
+    link.download = "convocatoria_OhSansi.pdf";
+    link.click();
+  };
+
   return (
-    <main className="w-full flex flex-col items-center justify-center px-4 md:px-16 py-10 text-onBack">
-      <section className="w-10/12 flex flex-col-reverse lg:flex-row items-center lg:space-x-10 mb-10 text-center ">
-        <div>
-          <h1 className="headline-lg text-primary mb-4">
-            OLIMPIADA CIENTÍFICA NACIONAL
-            <br /> SAN SIMÓN
-          </h1>
-          <p className="mb-6 text-md">
-            El Comité de la Olimpiadas Científica Nacional San Simón O! SanSi, a
-            través de la Facultad de Ciencias y Tecnología de la Universidad
-            Mayor de San Simón, convoca a los estudiantes del Sistema de
-            Educación Regular a participar en las Olimpiadas O! SanSi.
-          </p>
-          <div className="flex flex-col sm:flex-row gap-4 lg:gap-6 justify-center lg:justify-center">
-            <Button
-              label="Administrador"
-              className="lg:w-40"
-              onClick={() => {
-                navigate('/login');
-              }}
-            />
-            <Button
-              label="Olimpista"
-              className="lg:w-40"
-              onClick={() => {
-                localStorage.setItem('userRole', 'olympian');
-                navigate('/olympian');
-              }}
-            />
-          </div>
-        </div>
-        <img src="/assets/images/ohsansi2.png" className="mb-10 w-80" />
-      </section>{' '}
-      <section className="text-center mb-10 w-10/12">
-        <h1 className="headline-lg text-primary mb-4">Inscripción</h1>
-        <div className="flex flex-col items-start">
+    <main className="w-full min-h-screen flex flex-col items-center justify-start text-onBack bg-gradient-to-b from-white">
+      <Header />
+      <section className="text-center my-10 w-11/12 md:w-10/12 max-w-5xl">
+        <h1 className="text-3xl headline-lg text-primary mb-4 tracking-wide">Inscripción</h1>
+        <div className="bg-white shadow-md rounded-2xl px-6 py-6 text-left">
           {loading ? (
             <div className="w-full flex justify-center items-center h-36">
               <PageLoader />
             </div>
           ) : currentOlympiad ? (
             <>
-              <p className="text-base mb-1">
+              <p className="mb-2">
                 La fecha de inscripción inicia el{' '}
-                <span className="text-red-500 font-semibold">
-                  {formatDate(currentOlympiad.fecha_inicio)}
+                <span className="text-error body-lg">
+                  {formatDate(currentOlympiad.start_date)}
                 </span>
               </p>
-              <p className="text-base mb-1">
-                La fecha de finalización de inscripción será hasta el{' '}
-                <span className="text-red-500 font-semibold">
-                  {formatDate(currentOlympiad.fecha_fin)}
+              <p className="mb-2">
+                Finaliza el{' '}
+                <span className="text-error body-lg">
+                  {formatDate(currentOlympiad.end_date)}
                 </span>
               </p>
-              <p className="text-base mb-3">
-                El costo de inscripción será de{' '}
-                <span className="text-red-500 font-semibold">
-                  {currentOlympiad.costo} (Bs)
-                </span>{' '}
-                por área
-              </p>{' '}
+              <p className="mb-3">
+                Costo de inscripción: <span className="text-error body-lg">{currentOlympiad.cost} Bs</span> por área
+              </p>
               {(() => {
-                if (!currentOlympiad) return null;
                 const status = getRegistrationStatus(
-                  currentOlympiad.fecha_inicio,
-                  currentOlympiad.fecha_fin,
+                  currentOlympiad.start_date,
+                  currentOlympiad.end_date
                 );
-
-                const statusColors = {
-                  'not-started':
-                    'text-yellow-600 bg-yellow-50 border-yellow-200',
-                  active: 'text-success border-success bg-green-50',
-                  ended: 'text-red-600 bg-red-50 border-red-200',
-                } as const;
-
                 return (
-                  <div
-                    className={`w-full p-3 rounded-lg border-2 ${statusColors[status.status]}`}
-                  >
-                    <p className="text-base font-semibold text-center">
-                      {status.message}
-                    </p>
+                  <div className={`w-full p-4 mt-4 rounded-xl border-2 flex items-center justify-center gap-3 shadow-sm transition-all duration-300 ${statusColors[status.status]}`}>
+                    {icons[status.status]}
+                    <p className="subtitle-md">{status.message}</p>
                   </div>
                 );
               })()}
             </>
           ) : (
-            <p className="text-base mb-4">
-              No se pudo cargar la información de la olimpiada.
-            </p>
+            <p className="text-neutral">No se pudo cargar la información de la olimpiada.</p>
           )}
         </div>
       </section>
-      <section className="text-center mb-10 w-10/12">
-        <h1 className="headline-lg text-primary mb-4">Convocatoria</h1>
-        <div className="flex flex-col items-center text-md">
-          <p className="w-full text-start mb-4">
-            Puedes descargar la convocatoria de la olimpiada.
+
+      {currentOlympiad && (
+        <section className="text-center w-full py-16 px-6 md:px-20 bg-surface shadow-inner">
+          <h2 className="text-3xl headline-lg text-primary mb-4">Áreas Disponibles</h2>
+          <p className="body-lg text-onBack mb-8">
+            Conoce las áreas que se encuentran disponibles en la olimpiada:
           </p>
-          <div className="w-full lg:w-fit md:w-fit">
-            <Button
-              className="w-full"
-              label="Descargar Convocatoria"
-              variantColor="variantDesactivate"
-            />
-          </div>
+          {areasLoading ? (
+            <div className="w-full flex justify-center items-center h-36">
+              <PageLoader />
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
+              {availableAreas.map((area) => (
+                <CardArea key={area.area_id} areaName={area.area_name} />
+              ))}
+            </div>
+          )}
+        </section>
+      )}
+
+      <section className="flex flex-col justify-center text-center w-11/12 md:w-10/12 py-16 shadow-inner">
+        <h2 className="text-3xl headline-lg text-primary mb-4">Convocatoria</h2>
+        <p className="text-onBack mb-4 text-center body-lg">
+          Puedes descargar la convocatoria de la olimpiada.
+        </p>
+        <div className="w-full md:w-fit flex justify-center mx-auto">
+        <Button
+          className="w-full flex justify-center gap-2"
+          label="Descargar Convocatoria"
+          onClick={handleDownload}
+        />
         </div>
       </section>
-      <section className="text-center mb-10 w-10/12">
-        <h1 className="headline-lg text-primary mb-4">Prerrequisitos</h1>
-        <ol className="flex flex-col items-start list-decimal pl-4 space-y-2 text-md text-left">
-          <li>
-            Ser estudiante de nivel primaria o secundaria en el sistema de
-            Educación Regular del Estado Plurinacional de Bolivia.
-          </li>
+
+      <section className="text-center w-full py-16 px-6 md:px-20 mb-20 bg-white shadow-inner">
+        <h2 className="text-3xl headline-lg text-primary mb-4">Prerrequisitos</h2>
+        <div className='bg-white shadow-md rounded-2xl px-6 py-6 text-left'>
+        <ol className="text-left body-lg list-decimal pl-6 space-y-2 text-onBack">
+          <li>Ser estudiante de nivel primaria o secundaria en Bolivia.</li>
           <li>Registrar un tutor o profesor.</li>
-          <li>
-            Registrarse en el formulario de inscripción para el(las) área(s) que
-            se postula.
-          </li>
-          <li>
-            Cumplir los requisitos específicos de la categoría de competencia en
-            la que se inscribe.
-          </li>
-          <li>
-            Tener su documento de identificación personal vigente (cédula de
-            identidad) en el desarrollo de la competencia.
-          </li>
+          <li>Registrarse en el formulario para el(las) área(s) seleccionadas.</li>
+          <li>Cumplir los requisitos de la categoría correspondiente.</li>
+          <li>Tener su cédula de identidad vigente.</li>
           <li>Contar con correo electrónico personal o del tutor.</li>
         </ol>
+        </div>
       </section>
     </main>
   );
